d046cfab6b9b16fd9ecfb71f22dc3c0ebbba8cb1
Lin-Ya <fenghuiwing@outlook.com>
2018-05-16 21:20:54 +0800

    实现无限滚动获取数据、请求节流

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,20 +4,55 @@ import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
 
+//引入mint-ui使用无限滚动
+import { InfiniteScroll } from 'mint-ui';
+Vue.use(InfiniteScroll);
+
 let app = new Vue({
     el: '#app',
     data: {
-        lists: null
+        lists: null,
+        pageNum: 1,
+        pageSize: 6,
+        loading: false,
+        listsEnd: false
     },
+    // comments: {
+
+    // },
     created() {
-        axios.post(url.hotList, {
-            pageNum: 1,
-            pageSize: 6
-        }).then((res) => {
-            console.log(res)
-            this.lists = res.data.lists
-        }, (err) => {
-            console.log(err)
-        })
+        this.getLists()
+    },
+    methods: {
+        getLists() {
+            if (this.loading) {
+                return
+            } else if (this.listsEnd) {
+                return
+            }
+            this.loading = true
+            axios.post(url.hotList, {
+                pageNum: this.pageNum,
+                pageSize: this.pageSize
+            }).then((res) => {
+                if (this.lists) {
+                    let currentLists = res.data.lists
+                    this.lists = this.lists.concat(currentLists)
+                    //如果返回的条目小于预设的每页条数，说明已经到了数据库的底部了
+                    if (currentLists.length < this.pageSize) {
+                        this.listsEnd = true
+                    }
+                } else {
+                    //初次加载的时候，会来到这里
+                    this.lists = res.data.lists
+                }
+                this.loading = false
+                this.pageNum++
+            }, (err) => {
+                console.log(err)
+            })
+
+        }
     }
+
 })
