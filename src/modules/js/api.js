6e6f39991d9ea3024f9775f45bc709f91f3687ba
Lin-Ya <fenghuiwing@outlook.com>
2018-05-16 17:55:54 +0800

    引入vue处理首页，引入axios请求后端模拟数据

---
+++ src/modules/js/api.js
@@ -0,0 +1,10 @@
+let host = 'http://rapapi.org/mockjsdata/34221' //开发环境模拟接口
+
+let url = {
+    hotList: '/index/hotList'
+}
+for (key in url) {
+    url[key] = host + url[key]
+}
+
+module.exports = url;
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,15 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import './index.css'
+import 'css/common.css'
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import axios from 'axios'
+import url from 'js/api.js'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+let app = new Vue({
+    el: '#app',
+    data: {
+        lists: null
+    },
+    created() {
+        axios.post(url.hotList, {
+            pageNum: 1,
+            pageSize: 6
+        }).then((res) => {
+            console.log(res)
+            this.lists = res.data.lists
+        }, (err) => {
+            console.log(err)
+        })
+    }
 })
