762b9b05e6b608e22a29c4b9ea9e17a23809839a
Lin-Ya <fenghuiwing@outlook.com>
2018-05-16 14:51:47 +0800

    实现多页面配置

--- build/utils.js
+++ build/utils.js
@@ -3,7 +3,52 @@ const path = require('path')
 const config = require('../config')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const packageConfig = require('../package.json')
+var glob = require('glob')
+var HtmlWebpackPlugin = require('html-webpack-plugin')  //单独每个页面打包配置
+var PAGE_PATH = path.resolve(__dirname, '../src/pages') //设置读取的路径
+var merge = require('webpack-merge')  //webpack配置合拼
 
+//多入口配置
+exports.entries = function () {
+  var entryFiles = glob.sync(PAGE_PATH + '/*/*.js')
+  var map = {}
+  entryFiles.forEach((filePath) => {
+    var filename = filePath.substring(filePath.lastIndexOf('\/') + 1, filePath.lastIndexOf('.'))
+    map[filename] = filePath
+  })
+  return map
+}
+
+//多页面输出配置
+exports.htmlPlugin = function () {
+  var entryHtml = glob.sync(PAGE_PATH + '/*/*.html')
+  var arr = []
+  entryHtml.forEach((filePath) => {
+    var filename = filePath.substring(filePath.lastIndexOf('\/') + 1, filePath.lastIndexOf('.'))
+    var conf = {
+      template: filePath,
+      filename: filename + '.html',
+      chunks: [filename],
+      inject: true
+    }
+    if (process.env.NODE_ENV === 'production') {
+      conf = merge(conf, {
+        chunks: ['manifest', 'vendor', filename],
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true
+        },
+        chunksSortMode: 'dependency'
+      })
+    }
+    arr.push(new HtmlWebpackPlugin(conf))
+  })
+  return arr
+}
+
+
+//原来的配置
 exports.assetsPath = function (_path) {
   const assetsSubDirectory = process.env.NODE_ENV === 'production'
     ? config.build.assetsSubDirectory
