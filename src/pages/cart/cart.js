b44f7326d8174c4e19403eb63ccc56fc99118fcd
Lin-Ya <fenghuiwing@outlook.com>
2018-05-19 15:56:33 +0800

    完成商品店铺的选择和全选

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -19,20 +19,50 @@ new Vue({
             axios.post(url.cartList).then(res => {
                 let list = res.data.cartList
                 list.forEach(shop=>{
+                    shop.checked = true
                     shop.goodsList.forEach(good=>{
-                        good.checked = false
+                        good.checked = true
                     })
                 })
                 this.cartList = list
             })
         },
-        check(obj){
-            obj.checked = !obj.checked
+        goodcheck(shop,good){
+            good.checked = !good.checked
+            shop.checked = shop.goodsList.every(good=>{
+                return good.checked
+            })
+        },
+        shopcheck(shop){
+            shop.checked = !shop.checked
+            shop.goodsList.forEach(good=>{
+                good.checked = !good.checked                
+            })
+        },
+        selectAll(){
+            this.allChecked = !this.allChecked
         }
     },
     mixins: [mixin],
     computed: {
-
+        allChecked: {
+            get() {
+                if(this.cartList&&this.cartList.length){
+                    return this.cartList.every(shop=>{
+                        return shop.checked
+                    })
+                }
+                return false
+            },
+            set(val) {
+                this.cartList.forEach(shop=>{
+                    shop.checked = val;
+                    shop.goodsList.forEach(good=>{
+                        good.checked = val
+                    })
+                })
+            }
+        }
     },
     created() {
         this.getCartList()
