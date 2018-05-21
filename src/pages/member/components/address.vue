8104b08875c0e5bee5ec679d74eb69ceed039a9b
Lin-Ya <fenghuiwing@outlook.com>
2018-05-21 16:04:04 +0800

    首次router-link的使用

---
+++ src/pages/member/components/address.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    地址管理
+  </div>
+</template>
+
+<style scoped>
+  @import './address_base.css';
+  @import './address.css';
+</style>
+
+<script>
+export default {
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,7 +1,24 @@
-import './member_base.css'
-import './member.css'
 
 import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
-import Foot from 'components/Foot.vue'
+import Router from 'vue-router'
+//1. 注册使用router 组件
+Vue.use(Router)
+
+let routes = [{
+    path: '/',
+    components: require('./components/member.vue')
+},{
+    path: '/adress',
+    components: require('./components/address.vue')
+}]
+
+let router = new Router({
+    routes
+})
+new Vue({
+    el: '#app',
+    router
+    
+})
